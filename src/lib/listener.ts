330ceb4ff7a6c2508e3ad8ae549625bddc532e25
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2019-12-11 15:48:20 -0500

    fix: proper typing for notification handler

--- src/lib/listener.ts
+++ src/lib/listener.ts
@@ -1,9 +1,10 @@
-import { Client } from 'pg';
+import { Client, Notification } from 'pg';
 import config from './config';
 
 const client = new Client({ connectionString: config.databaseUrl });
+type Listener = (msg: Notification) => void;
 
-export const registerListener = async (channel: string, fns: Function[]) => {
+export const registerListener = async (channel: string, fns: Listener[]) => {
   for (let fn of fns) {
     client.on('notification', fn);
   }
