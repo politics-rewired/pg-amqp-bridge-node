16bc870e0a8f94e0af8cc726212a457dfa83887d
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2021-03-04 09:23:27 -0500

    feat(reconnect): additional logging and defensiveness around restart

--- src/lib/listener.ts
+++ src/lib/listener.ts
@@ -2,17 +2,19 @@ import { Notification } from 'pg';
 import config from './config';
 import createSubscriber from 'pg-listen';
 
-const subscriber = createSubscriber(
-  { connectionString: config.databaseUrl },
-  // by default, pg-listen assumes your payload is JSON
-  // we use a custom format - ${routingKey}|${jobPayload}
-  // parsing this format is handled elsewhere in in the publisher / acker
-  { parse: s => s }
-);
-
 type Listener = (msg: Notification) => void;
 
 export const registerListener = async (channel: string, fns: Listener[]) => {
+  console.log(`Attempting to connect to ${channel}`);
+
+  const subscriber = createSubscriber(
+    { connectionString: config.databaseUrl },
+    // by default, pg-listen assumes your payload is JSON
+    // we use a custom format - ${routingKey}|${jobPayload}
+    // parsing this format is handled elsewhere in in the publisher / acker
+    { parse: s => s }
+  );
+
   for (let fn of fns) {
     subscriber.events.on('notification', fn);
   }
@@ -21,6 +23,10 @@ export const registerListener = async (channel: string, fns: Listener[]) => {
     console.error('Client got fatal error', err);
     console.log('Shutting down...');
     process.exit();
+  });
+
+  subscriber.events.on('reconnect', attempt => {
+    console.log(`Attempting reconnect - attempt ${attempt}`);
   });
 
   await subscriber.connect();
